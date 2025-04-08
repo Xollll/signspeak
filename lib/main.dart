d09b1883fea2ac3106a0af9d5f046fee06ee06a2
Xollll <mzlkrnainn@gmail.com>
2025-04-08 20:26:44 +0800

    simple commit update library main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:signspeak/communication/communicator.dart';
-import 'package:signspeak/tutorialapp';
+import 'package:signspeak/tutorialapp.dart';
 import 'package:signspeak/tutorialpage/tutorial.dart';
 import 'firebase_options.dart';
 import 'package:signspeak/dashboard/homepage.dart';
